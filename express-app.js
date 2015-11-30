722ef211fae8b4bbbb9a86be33b41b6b7666a09f
Queixal <mmi.developer@gmail.com>
2015-11-30 21:13:48 +0100

    Se hace funcional la API

---
+++ express-app.js
@@ -0,0 +1,77 @@
+var express = require("express");
+var app = express();
+var http = require("http");
+var server = http.createServer(app);
+var fs = require("fs");
+var path = require("path");
+var bodyParser = require("body-parser");
+
+app.use(bodyParser.json());
+var objectIds = {
+    friendId: 0
+}
+
+var friends = [];
+
+app.post("/api/v1/friends", function(req, res) {
+    req.body.id= ++objectIds.friendId;
+    friends.push(req.body);
+    res.send(JSON.stringify({id:req.body.id}));
+});
+
+app.get("/api/v1/friends", function(req, res) {
+    res.set("Content-type", "application/json")
+    res.send(JSON.stringify(friends));
+});
+
+app.put("/api/v1/friends/:id", function(req, res) {
+    var friend = friends.filter(f=>f.id==req.params.id)[0];
+    friend.nickname =  req.body.nickname;
+    res.end();
+});
+
+
+app.get("/*", function(req, res) {
+    var filePath = req.originalUrl;
+    
+    if (filePath === "/")
+        filePath = "./index.html";
+    else {
+        filePath = "./" + filePath;
+    }
+    
+    var indexOfQuerySymbol = filePath.indexOf("?");
+    
+    if (indexOfQuerySymbol > -1) {
+        filepath = filePath.substring(0, indexOfQuerySymbol);
+    }
+    
+    switch (path.extname(filePath)) {
+    	case ".inc":
+        case ".html":
+            res.set("Content-type", "text/html");
+            break;
+
+        case ".js":
+            res.set("Content-type", "text/javascript");
+            break;
+
+        case ".css":
+            res.set("Content-type", "text/css");
+            break;
+
+        case ".wof":
+        case ".wof2":
+            res.set("Content-type", "application/font-woff");
+            break;
+    
+    }
+    
+    res.send(fs.readFileSync(filePath, "utf8"));
+}
+);
+
+server.listen(8888, function() {
+    console.log("Node server running on http://localhost:8888");
+}
+);
--- js/controllers/friendlistController.js
+++ js/controllers/friendlistController.js
@@ -9,17 +9,39 @@
 import {controllers} from "/js/modules";
 
 //Controladora para el friendList
-controllers.controller("friendListController", function($scope, $stateParams) {
-    $scope.friends = [
-                    {nickname : "Matias", description : "Write less do more"},
-                    {nickname : "cacaman", description : "I.. AM.. CACAMAN!"}
-                    ];
+controllers.controller("friendListController", function($scope, $stateParams, $http) {
+    
+    $scope.friends = [];
+    
+    $http.get("/api/v1/friends").then(function(response){
+        $scope.friends = response.data;
+    });
+
+    $scope.modifyFriend = function() {
+        $http.put("/api/v1/friends/"+$scope.selectedFriend.id);
+    }
+
+    $scope.newFriendName = null;
+    $scope.selectedFriend = {nickname: ""};
+
+    $scope.addFriend = function() {
+        var friendToAdd = {nickname: $scope.newFriendName};
+        $http.post("/api/v1/friends", friendToAdd)
+        .then(function(response) {
+            friendToAdd.id = response.data.id;
+            $scope.friends.push(friendToAdd);
+        });
+    }
+
+    $scope.selectedFriend = null;
+
+    $scope.selectFriend = friend =>  $scope.selectedFriend = friend;
+
     $scope.friend=null;
     if ($stateParams.hasOwnProperty("nickname")) {
         $scope.friend= $scope.friends.filter(f => f.nickname == $stateParams.nickname)[0];
     }
-    
 });
 
 //inyectamos los parámetros de entrada para el controler
-controllers.$inject = ["$scope", "$stateParams"]
+controllers.$inject = ["$scope", "$stateParams", "$http"]
